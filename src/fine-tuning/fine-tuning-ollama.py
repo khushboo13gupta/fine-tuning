729543ffdea086281362d7461f913dc77ab268eb
Khushboo Gupta <khushboo13gupta@gmail.com>
2026-09-24 17:27:26 -0700

    Implement Hugging Face and W&B login and config
    Added login functionality for Hugging Face and Weights & Biases, configured logging, and set up model training parameters.

--- src/fine-tuning/fine-tuning-ollama.py
+++ src/fine-tuning/fine-tuning-ollama.py
@@ -18,7 +18,7 @@ import matplotlib.pyplot as plt
 # Constants
 
 BASE_MODEL = "meta-llama/Llama-3.2-3B"
-PROJECT_NAME = "price"
+PROJECT_NAME = "<put your project name here>"
 HF_USER = "<put your hugging face user name here>" # your HF name here!
 
 DATA_USER = "<put user name where data resides>"
@@ -62,3 +62,114 @@ LOG_STEPS = 5 if LITE_MODE else 10
 SAVE_STEPS = 100 if LITE_MODE else 200
 LOG_TO_WANDB = True
 
+# Log in to HuggingFace
+
+hf_token = userdata.get('HF_TOKEN')
+login(hf_token, add_to_git_credential=True)
+
+# Log in to Weights & Biases
+wandb_api_key = userdata.get('WANDB_API_KEY')
+os.environ["WANDB_API_KEY"] = wandb_api_key
+wandb.login()
+
+# Configure Weights & Biases to record against our project
+os.environ["WANDB_PROJECT"] = PROJECT_NAME
+os.environ["WANDB_LOG_MODEL"] = "false"
+os.environ["WANDB_WATCH"] = "false"
+
+dataset = load_dataset(DATASET_NAME)
+train = dataset['train']
+val = dataset['val'].select(range(VAL_SIZE))
+test = dataset['test']
+
+if LOG_TO_WANDB:
+  wandb.init(project=PROJECT_NAME, name=RUN_NAME)
+
+# pick the right quantization
+
+if QUANT_4_BIT:
+  quant_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_compute_dtype=torch.bfloat16 if use_bf16 else torch.float16,
+    bnb_4bit_quant_type="nf4"
+  )
+else:
+  quant_config = BitsAndBytesConfig(
+    load_in_8bit=True,
+    bnb_8bit_compute_dtype=torch.bfloat16 if use_bf16 else torch.float16,
+  )
+
+# Load the Tokenizer and the Model
+tokenizer = AutoTokenizer.from_pretrained(BASE_MODEL, trust_remote_code=True)
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = "right"
+
+base_model = AutoModelForCausalLM.from_pretrained(
+    BASE_MODEL,
+    quantization_config=quant_config,
+    device_map="auto",
+)
+base_model.generation_config.pad_token_id = tokenizer.pad_token_id
+
+# LoRA Parameters
+
+lora_parameters = LoraConfig(
+    lora_alpha=LORA_ALPHA,
+    lora_dropout=LORA_DROPOUT,
+    r=LORA_R,
+    bias="none",
+    task_type="CAUSAL_LM",
+    target_modules=TARGET_MODULES,
+)
+
+# Training parameters
+
+train_parameters = SFTConfig(
+    output_dir=PROJECT_RUN_NAME,
+    num_train_epochs=EPOCHS,
+    per_device_train_batch_size=BATCH_SIZE,
+    per_device_eval_batch_size=1,
+    gradient_accumulation_steps=GRADIENT_ACCUMULATION_STEPS,
+    optim=OPTIMIZER,
+    save_steps=SAVE_STEPS,
+    save_total_limit=10,
+    logging_steps=LOG_STEPS,
+    learning_rate=LEARNING_RATE,
+    weight_decay=0.001,
+    fp16=not use_bf16,
+    bf16=use_bf16,
+    max_grad_norm=0.3,
+    max_steps=-1,
+    warmup_ratio=WARMUP_RATIO,
+    group_by_length=True,
+    lr_scheduler_type=LR_SCHEDULER_TYPE,
+    report_to="wandb" if LOG_TO_WANDB else None,
+    run_name=RUN_NAME,
+    max_length=MAX_SEQUENCE_LENGTH,
+    save_strategy="steps",
+    hub_strategy="every_save",
+    push_to_hub=True,
+    hub_model_id=HUB_MODEL_NAME,
+    hub_private_repo=True,
+    eval_strategy="steps",
+    eval_steps=SAVE_STEPS
+)
+
+fine_tuning = SFTTrainer(
+    model=base_model,
+    train_dataset=train,
+    eval_dataset=val,
+    peft_config=lora_parameters,
+    args=train_parameters
+)
+
+# Fine-tune!
+fine_tuning.train()
+
+# Push our fine-tuned model to Hugging Face
+fine_tuning.model.push_to_hub(PROJECT_RUN_NAME, private=True)
+print(f"Saved to the hub: {PROJECT_RUN_NAME}")
+
+if LOG_TO_WANDB:
+  wandb.finish()
